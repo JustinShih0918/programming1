f928b6780221eb8843e8c3a296fb1d7777eab728
JustinShih0918 <72808967+JustinShih0918@users.noreply.github.com>
2023-11-23 15:59:44 +0800

    practice -- lab8 second question

--- output/week1/Yang_hw1/practice.c
+++ output/week1/Yang_hw1/practice.c
@@ -1,44 +1,52 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
+int r,c,k,m;
+int dom[102][102];
+int rec[102][102];
+int x[4] = {-1,1,0,0};
+int y[4] = {0,0,1,-1};
+void pA(int a[102][102]);
+int main(void){
+    memset(dom,-1,sizeof(dom));
+    memset(rec,0,sizeof(rec));
 
-#define MAXN 505 
-
-int record[MAXN][MAXN];
-char s[MAXN];
-
-int solve(int l, int r) {
-    // return 1 if s[l, r] is valid, 0 otherwise.
-    if (record[l][r] != -1) return record[l][r]; // If the substring is computed, return it immediately.
-    int flag = 0; // store your answer for this state in flag.
-    // [TODO] Implement your codes here.
-    // Noticed that you only need to store the result in "flag".
-    if((r-l)>1){
-        for(int i = l;i<r;i++){
-            if(solve(l,i)&&solve(i+1,r)){
-                flag = 1;
-                break;
+    scanf("%d %d %d %d",&r,&c,&k,&m);
+    for(int i = 1;i<=r;i++){
+        for(int j = 1;j<=c;j++){
+            scanf("%d",&dom[i][j]);
+        }
+    }
+    while(m--){
+        for(int i = 1;i<=r;i++){
+            for(int j = 1;j<=c;j++){
+                int val = 0;
+                if(dom[i][j]!=-1) val = dom[i][j]/k;
+                //else if(dom[i][j]%k!=0&&dom[i][j]!=-1) val = dom[i][j]/k+1;
+                for(int d = 0;d<4;d++){
+                    if(dom[i+x[d]][j+y[d]]!=-1){
+                        rec[i+x[d]][j+y[d]]+= val;
+                        rec[i][j] -= val;
+                    }
+                }
             }
         }
-        if(solve(l+1,r-1)&&s[l]=='('&&s[r]==')') flag = 1;
+
+        for(int i = 1;i<=r;i++){
+            for(int j = 1;j<=c;j++){
+                dom[i][j] += rec[i][j];
+            }
+        }
+        memset(rec,0,sizeof(rec));
     }
-    else if(s[l]=='('&&s[r]==')') flag = 1;
-    else if(l==r&&s[r]==' ') flag =1;
-    // End 
-    record[l][r] = flag;
-    return flag;
+    pA(dom);
 }
 
-int main () {
-    int T;
-    scanf("%d", &T);
-    while (T--) {
-        for (int i = 0;i < MAXN; ++i)
-            for (int j = 0;j < MAXN; ++j)
-                record[i][j] = -1;
-        scanf("%s", s);
-        int flag = solve(0, strlen(s)-1);
-        printf(flag ? "Yes\n" : "No\n");
+void pA(int a[102][102]){
+    for(int i = 1;i<=r;i++){
+        printf("%d",a[i][1]);
+        for(int j = 2;j<=c;j++){
+            printf(" %d",a[i][j]);
+        }
+        printf("\n");
     }
-    return 0;
 }
