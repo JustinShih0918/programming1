07dbcf7cbec3c87b5695feab84aaa01a9257ba86
JustinShih0918 <72808967+JustinShih0918@users.noreply.github.com>
2023-12-05 16:45:19 +0800

    update var name in hw9 most valuable sentance

--- output/hw9/output/most_valuable_sentence.c
+++ output/hw9/output/most_valuable_sentence.c
@@ -1,68 +1,68 @@
-#include <string.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 #include <ctype.h>
 
+#define MAXL 100005
+#define MAXN 1005
+
 int n;
-char key[1005];
-int cnt[1005];
-char person[100005];
-char temp1[100005];
-char temp2[100005];
-char temp3[100005];
-char maxSen[100001];
-char maxName[100001];
-const char s[6] = " ,.!?";
+char key[MAXN];
+int cnt[MAXN];
+char person[MAXL];
+char tempName[MAXL];
+char tempSen[MAXL];
+char temp[MAXL];
 char *token;
+char s[6] = " ,!?.";
+char ansPerson[MAXL];
+char ansSen[MAXL];
+int max = 0;
 
-
-int main(void){
-    
+int main(){
     scanf("%d %s\n",&n,key);
     int klen = strlen(key);
-    int max = 0;
-    
     for(int i = 0;i<n;i++){
-        if(fgets(person,100005,stdin)!=NULL){
-            memset(temp1,'\0',sizeof(temp1));
-            memset(temp2,'\0',sizeof(temp2));
-            memset(temp3,'\0',sizeof(temp3));
+        if(fgets(person,MAXL,stdin)!=NULL){
+            memset(tempName,'\0',sizeof(tempName));
+            memset(tempSen,'\0',sizeof(tempSen));
+            memset(temp,'\0',sizeof(temp));
             int len = strlen(person);
             int pos = 0;
             for(int j = 0;j<len;j++){
-                if(person[j]==':') pos = j;
+                if(person[j]==':'){
+                    pos = j;
+                    break;
+                }
             }
-            strncpy(temp1,person,pos);
-            strcpy(temp2,&person[pos+1]);
-            strcpy(person,temp1);
-            strcpy(temp3,temp2);
-            token = strtok(temp2,s);
+            strncpy(tempName,person,pos);
+            strcpy(tempSen,&person[pos+1]);
+            strcpy(temp,tempSen);
+            //printf("%s\n%s\n%s\n",tempName,tempSen,temp);
+            
+            token = strtok(temp,s);
             while(token!=NULL){
                 int tlen = strlen(token);
                 int isS = 0;
-                if(tlen == klen){
+                if(tlen==klen){
                     for(int k = 0;k<tlen;k++){
-                        if((key[k]==token[k])||(toupper(key[k])==token[k])||(tolower(key[k])==token[k])){
+                        if((token[k]==key[k])||(toupper(token[k])==key[k])||(tolower(token[k])==key[k])){
                             isS = 1;
-                        }
-                        else{
+                        }else{
                             isS = 0;
                             break;
                         }
                     }
+        
                 }
                 if(isS) cnt[i]++;
                 token = strtok(NULL,s);
             }
-        } 
-        if(cnt[i]>max){
-            max = cnt[i];
-            strcpy(maxName,person);
-            strcpy(maxSen,temp3);
         }
-        
+        if(cnt[i]>max){
+            strcpy(ansPerson,tempName);
+            strcpy(ansSen,tempSen);
+            max = cnt[i];
+        }
     }
-
-
-    printf("%s: %s",maxName,maxSen);
+    printf("%s: %s",ansPerson,ansSen);
 }
