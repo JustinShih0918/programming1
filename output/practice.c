575ba1548e9ac0e0770262512db6cd136bc6c446
JustinShih0918 <72808967+JustinShih0918@users.noreply.github.com>
2023-12-19 17:09:40 +0800

    nothing important

--- output/practice.c
+++ output/practice.c
@@ -5,7 +5,9 @@
 int row[50][50];
 int col[50][50];
 char grid[50][50];
+
 int N,M;
+int T;
 
 int check(){
     for(int i = 0;i<N;i++){
@@ -15,7 +17,7 @@ int check(){
             if(grid[i][j]=='o') cnt++;
             else if(cnt>0){
                 if(idx==row[i][0]) return 0;
-                if(cnt!=row[i][++idx]) return 0;
+                if(row[i][++idx]!=cnt) return 0;
                 cnt = 0;
             }
         }
@@ -29,7 +31,7 @@ int check(){
             if(grid[j][i]=='o') cnt++;
             else if(cnt>0){
                 if(idx==col[i][0]) return 0;
-                if(cnt!=col[i][++idx]) return 0;
+                if(col[i][++idx]!=cnt) return 0;
                 cnt = 0;
             }
         }
@@ -37,36 +39,32 @@ int check(){
     }
 
     return 1;
-
 }
 
 int main(){
-    int T;
     scanf("%d",&T);
     while(T--){
         memset(row,0,sizeof(row));
         memset(col,0,sizeof(col));
         memset(grid,'\0',sizeof(grid));
         scanf("%d %d",&N,&M);
-        
         for(int i = 0;i<N;i++){
             scanf("%d",&row[i][0]);
             for(int j = 1;j<=row[i][0];j++){
                 scanf("%d",&row[i][j]);
             }
         }
-
         for(int i = 0;i<M;i++){
             scanf("%d",&col[i][0]);
             for(int j = 1;j<=col[i][0];j++){
                 scanf("%d",&col[i][j]);
             }
         }
-
         for(int i = 0;i<N;i++){
             scanf("%s",grid[i]);
         }
 
         printf(check()?"Yes\n":"No\n");
     }
+
 }
