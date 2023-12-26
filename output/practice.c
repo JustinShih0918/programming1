a2a789a0d73172c04937d347e521ff15c2a6e0f3
JustinShih0918 <72808967+JustinShih0918@users.noreply.github.com>
2023-12-26 16:36:24 +0800

    flip game v2 update

--- output/practice.c
+++ output/practice.c
@@ -1,48 +1,67 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#define max(x,y) ((x)>(y)?(x):(y))
-int N;
-long long price[100000];
 
-long long recursion(int x,int hold){
-    if(x>=N) return 0;
-    if(!hold){
-        long long buy = recursion(x+1,1)-price[x];
-        long long not_buy = recursion(x+1,0);
-        return max(buy,not_buy);
+int N,M;
+int grid[20][20];
+int MIN;
+int cnt;
+int x[5] = {1,-1,0,0,0};
+int y[5] = {0,0,1,-1,0};
+
+int check(){
+    for(int i = 1;i<=N;i++){
+        for(int j = 1;j<=M;j++){
+            if(grid[i][j]!=grid[1][1]) return 0;
+        }
     }
-    else{
-        long long sell = recursion(x+1,0)+price[x];
-        long long not_sell = recursion(x+1,1);
-        return max(sell,not_sell);
+    return 1;
+}
+
+void flip(int i,int j){
+    for(int k = 0;k<5;k++){
+        grid[i+x[k]][j+y[k]] = !grid[i+x[k]][j+y[k]];
     }
 }
 
-long long record(){
-    long long hold = -2e9;
-    long long nothold = 0;
-    for(int i = 0;i<N;i++){
-        long now_hold = max(hold,nothold-price[i]);
-        long now_nothold = max(nothold,hold+price[i]);
-        hold = now_hold;
-        nothold = now_nothold;
-    }
-    return max(hold,nothold);
+
+void dfs(int id){
+    if(MIN>cnt&&check()) MIN = cnt;
+    if(id==N*M) return;
+
+    //no flip
+    dfs(id+1);
+    if(cnt+1>=MIN) return;
+    //flip
+    flip(id/M+1,id%M+1);
+    cnt++;
+    dfs(id+1);
+    flip(id/M+1,id%M+1);
+    cnt--;
 }
 
-long long greed(){
-    long long p = 0;
-    for(int i = 1;i<N;i++){
-        if(price[i]>price[i-1]) p+=price[i]-price[i-1];
-    }
-    return p;
+void solve(){
+    MIN = 7122;
+    cnt = 0;
+    dfs(0);
+    if(MIN==7122) printf("oops\n");
+    else printf("%d\n",MIN);
 }
+
 
 int main(){
-    scanf("%d",&N);
-    for(int i = 0;i<N;i++){
-        scanf("%d",&price[i]);
+    int T;
+    scanf("%d",&T);
+    while(T--){
+        scanf("%d %d",&N,&M);
+        char tmp;
+        for(int i = 1;i<=N;i++){
+            for(int j = 1;j<=M;j++){
+                scanf(" %c",&tmp);
+                if(tmp=='w') grid[i][j] = 1;
+                else if(tmp=='b') grid[i][j] = 0;
+            }
+        }
+
+        solve();
     }
-    printf("%lld\n",greed());
 }
